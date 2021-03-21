1892ed2ccb6675df5d7f8368d7b7aaacc2bd82c2
Steve Quadros <stevequadros@gmail.com>
2021-03-21 15:09:04 -0400

    Update plan to print overview as well

--- main.go
+++ main.go
@@ -14,6 +14,11 @@ import (
 var intervals = []int{2,4,8,16}
 var startDate = time.Now()
 var questionsPerDay = 3
+var Difficulty = map[string]int{
+	"Easy": 1,
+	"Medium": 2,
+	"Hard": 3,
+}
 
 type Question struct {
 	Link, Title, Difficulty string
@@ -26,37 +31,58 @@ type Questions struct {
 
 func main() {
 	c := csv.NewReader(os.Stdin)
-	schedule := map[string]Questions{}
-
-	var curQuestions = 0
-	var curDate = startDate
 	// skip header
 	_, err := c.Read()
 	if err != nil {
 		panic(err)
 	}
+	
+	var allQuestions []Question
 	for line, err := c.Read(); err != io.EOF; line, err = c.Read() {
 		if err != nil {
 			panic(err)
 		}
+		allQuestions = append(allQuestions, Question{Title: line[0], Link: line[1], Difficulty: line[2]})
+	}
 
-		// go to next day if we've reached max new questions for this day
+	if len(allQuestions) < 1 {
+		fmt.Println("no questions to schedule")
+		return
+	}
+
+	// sort by difficult
+	sort.Slice(allQuestions, func(i,j int) bool { 
+		return Difficulty[allQuestions[i].Difficulty] < Difficulty[allQuestions[j].Difficulty]
+	})
+
+	var schedule = map[string]*Questions{}
+	var curQuestions = 0
+	var curDate = startDate
+	for _, q := range allQuestions {
 		if curQuestions > questionsPerDay-1 {
 			curDate = curDate.AddDate(0,0,1)
 			curQuestions = 0
 		}
 		curQuestions++
-		question := Question{Title: line[0], Link: line[1], Difficulty: line[2]}
-		questions := schedule[formatTime(curDate)] 
-		questions.New = append(questions.New, question)
-		schedule[formatTime(curDate)] = questions
+
+		dateKey := formatTime(curDate)
+		_, ok := schedule[dateKey]
+		if !ok {
+			schedule[dateKey] = &Questions{}
+		}
+		questions := schedule[dateKey]
+		questions.New = append(questions.New, q)
 
 		// for each interval add interval to innerDate and add question to map[innerdate]
 		for _, i := range intervals {
 			tempDate := curDate.AddDate(0,0,i-1)
-			reviews := schedule[formatTime(tempDate)]
-			reviews.Review = append(reviews.Review, question)
-			schedule[formatTime(tempDate)] = reviews 
+			dateKey := formatTime(tempDate)
+			_, ok := schedule[dateKey]
+			if !ok {
+				schedule[dateKey] = &Questions{}
+			}
+			questions := schedule[dateKey]
+			questions.Review = append(questions.Review, q)
 		}
 	}
 	
@@ -66,31 +92,36 @@ func main() {
 	}
 	sort.Strings(dates)
 
-	b := &strings.Builder{}
+	details := &strings.Builder{}
+	overview := &strings.Builder{}
+	fmt.Fprintln(overview, "Overview:")
 	var max int
 	var maxDate string
 	for _, date := range dates {
 		questions := schedule[date]
 		new := questions.New
 		review := questions.Review
-		fmt.Fprintln(b, date)
+		fmt.Fprintln(details, date)
 		curTotal := len(new) + len(review)
-		fmt.Fprintln(b, "Total Problems", curTotal)
+
+		fmt.Fprintln(details, "Total Problems", curTotal)
+		fmt.Fprintf(overview, "Date: %s, NumQuestions: %d\n", date, curTotal)
+
 		if curTotal > max {
 			max = curTotal
 			maxDate = date
 		}
 
-		fmt.Fprintln(b, "New: ", len(new))
+		fmt.Fprintln(details, "New: ", len(new))
 		for _, q := range new {
-			fmt.Fprintln(b, q.Title, " - ", q.Difficulty)
+			fmt.Fprintln(details, q.Title, " - ", q.Difficulty)
 		}
 
-		fmt.Fprintln(b, "Review: ", len(review))
+		fmt.Fprintln(details, "Review: ", len(review))
 		for _, q := range review {
-			fmt.Fprintln(b, q.Title, " - ", q.Difficulty)
+			fmt.Fprintln(details, q.Title, " - ", q.Difficulty)
 		}
-		fmt.Fprintln(b)
+		fmt.Fprintln(details)
 	}
 
 	header := &strings.Builder{}
@@ -103,9 +134,10 @@ func main() {
 	fmt.Fprintln(header)
 	fmt.Fprintf(header, "Questions Per Day: %d\n", questionsPerDay)
 	fmt.Fprintf(header, "Max questions: %d on %s\n", max, maxDate)
-	fmt.Println(header.String())
 
-	fmt.Println(b.String())
+	fmt.Println(header.String())
+	fmt.Println(overview.String())
+	fmt.Println(details.String())
 }
 
 func formatTime(t time.Time) string {
